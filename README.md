f88aeb83de49fb3ec3accdf574aee1801f095a39
Jarod Castillo <43973221+Smotto@users.noreply.github.com>
2025-06-08 22:43:14 -0700

    refactor with hashmaps

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-* Automatically detects where your Lost Ark's Customizing path is.
-* Automatically detects your local region
-* Automatically shows files that are not converted to the selected region
+### This is still in beta and being updated.
+* Detects where your Lost Ark's Customizing path is located
+* Detects your local region (without internet)
+* Shows files that are not converted to the selected region
 * Aesthetic art
-* 
+* < 1 MB executable
+
+### Foreseeable Features
+* Directory listening with automation (checked by user first and persists)
+* Exporting files to your customizations to a folder of your choosing.
+
+
+### Additional Resources
+#### Presets
+* 🇰🇷 KOR: https://discord.com/channels/943866124292980796/944238985121787964
+* USA: https://discord.com/channels/212635560596996097/943796313257037824
+* RUS: https://discord.com/channels/567277753607651338/1207978460568485899
--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,30 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	_putenv("SLINT_BACKEND=winit-skia");
 	auto                              ui               = AppWindow::create();
+
 	const std::unique_ptr<CusManager> cus_file_manager = std::make_unique<CusManager>();
 
-	if (!cus_file_manager->LoadFiles()) {
-		DEBUG_LOG("Failed to load .cus files!");
-		return EXIT_FAILURE;
-	}
+	auto                              initial_region   = slint::SharedString(OperatingSystemFunctions::GetLocalizationRegion());
+	ui->global<GlobalVariables>().set_local_region(initial_region);
+	ui->global<GlobalVariables>().set_selected_region(initial_region);
 
-	ui->global<GlobalVariables>().set_local_region(slint::SharedString(OperatingSystemFunctions::GetLocalizationRegion()));
-	ui->global<GlobalVariables>().set_selected_region(slint::SharedString(OperatingSystemFunctions::GetLocalizationRegion()));
-	auto selected_region = ui->global<GlobalVariables>().get_selected_region();
-	cus_file_manager->RefreshUnconvertedFiles(selected_region.data());
+	cus_file_manager->RefreshUnconvertedFiles(initial_region.data());
 	ui->global<GlobalVariables>().set_unconverted_files(cus_file_manager->GetSlintModelUnconvertedFiles());
 
-	ui->global<GlobalVariables>().on_request_refresh_files([&cus_file_manager, &ui]() {
-		auto selected_region = ui->global<GlobalVariables>().get_selected_region();
-		cus_file_manager->RefreshUnconvertedFiles(selected_region.data());
+	ui->global<GlobalVariables>().on_request_refresh_files([&cus_file_manager, &ui]() -> bool {
+		const auto region = ui->global<GlobalVariables>().get_selected_region();
+		cus_file_manager->RefreshUnconvertedFiles(region.data());
+		return true;
 	});
 
-	ui->global<GlobalVariables>().on_convert_files([&cus_file_manager, &ui]() {
-		auto region = ui->global<GlobalVariables>().get_selected_region();
-		cus_file_manager->ConvertFilesToRegion(region.data());
+	ui->global<GlobalVariables>().on_convert_files([&cus_file_manager, &ui]() -> bool {
+		const auto region = ui->global<GlobalVariables>().get_selected_region();
+		if (!cus_file_manager->ConvertFilesToRegion(region.data())) {
+			DEBUG_LOG("How did the write fail?");
+			return false;
+		}
 		cus_file_manager->RefreshUnconvertedFiles(region.data());
-		cus_file_manager->SaveModified();
+		return true;
 	});
 
 	ui->run();
